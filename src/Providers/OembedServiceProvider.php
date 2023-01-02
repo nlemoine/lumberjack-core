79de914772bbc05d89f950f3561652492030ea3c
Nicolas Lemoine <nico.lemoine@gmail.com>
2023-01-02 13:09:26 +0100

    WIP

--- src/Providers/OembedServiceProvider.php
+++ src/Providers/OembedServiceProvider.php
@@ -33,7 +33,7 @@ class OembedServiceProvider extends ServiceProvider
     public function setOembedTtl($url, $attr, $postId)
     {
         // Set to 0 for debugging, will fetch the oembed data on every request
-        return MONTH_IN_SECONDS;
+        return $this->getConfig('oembed.ttl', MONTH_IN_SECONDS);
     }
 
     /**
