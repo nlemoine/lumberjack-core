81c32f396625a8948caaf86b98137596067df373
Nicolas Lemoine <nico.lemoine@gmail.com>
2023-03-07 21:58:58 +0100

    WIP

--- src/Helpers.php
+++ src/Helpers.php
@@ -9,7 +9,6 @@ use Rareloop\Lumberjack\Facades\Log;
 use Rareloop\Lumberjack\Facades\Router;
 use Rareloop\Lumberjack\Facades\Session;
 use Rareloop\Lumberjack\Http\Responses\RedirectResponse;
-use Rareloop\Lumberjack\Http\Responses\TimberResponse;
 
 class Helpers
 {
@@ -39,48 +38,15 @@ class Helpers
         return Config::get($key, $default);
     }
 
-    public static function view($template, $context = [], $statusCode = 200, $headers = [])
-    {
-        return new TimberResponse($template, $context, $statusCode, $headers);
-    }
-
     public static function route($name, $params = [])
     {
         return Router::url($name, $params);
-    }
-
-    public static function redirect($url, $statusCode = 302, $headers = [])
-    {
-        return new RedirectResponse($url, $statusCode, $headers);
-    }
-
-    public static function report(Exception $e)
-    {
-        return static::app(ExceptionHandlerContract::class)->report($e);
     }
 
     public static function request()
     {
         return static::app('request');
     }
-
-    // public static function session($key = null, $default = null)
-    // {
-    //     if (is_null($key)) {
-    //         return static::app('session');
-    //     }
-
-    //     if (is_array($key)) {
-    //         return Session::put($key);
-    //     }
-
-    //     return Session::get($key, $default);
-    // }
-
-    // public static function back()
-    // {
-    //     return static::redirect(static::session()->previousUrl());
-    // }
 
     public static function logger($message = null, $context = [])
     {
--- src/functions.php
+++ src/functions.php
@@ -16,47 +16,12 @@ if (!function_exists('config')) {
     }
 }
 
-if (!function_exists('view')) {
-    function view()
-    {
-        return call_user_func_array([Helpers::class, 'view'], func_get_args());
-    }
-}
-
 if (!function_exists('route')) {
     function route()
     {
         return call_user_func_array([Helpers::class, 'route'], func_get_args());
     }
 }
-
-if (!function_exists('redirect')) {
-    function redirect()
-    {
-        return call_user_func_array([Helpers::class, 'redirect'], func_get_args());
-    }
-}
-
-if (!function_exists('report')) {
-    function report()
-    {
-        return call_user_func_array([Helpers::class, 'report'], func_get_args());
-    }
-}
-
-// if (!function_exists('session')) {
-//     function session()
-//     {
-//         return call_user_func_array([Helpers::class, 'session'], func_get_args());
-//     }
-// }
-
-// if (!function_exists('back')) {
-//     function back()
-//     {
-//         return call_user_func_array([Helpers::class, 'back'], func_get_args());
-//     }
-// }
 
 if (!function_exists('request')) {
     function request()
