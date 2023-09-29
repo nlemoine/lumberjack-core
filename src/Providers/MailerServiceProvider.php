960f6f02d34d1e90f8e0da3709df151f689a641d
Nicolas Lemoine <nico.lemoine@gmail.com>
2023-09-29 17:17:01 +0200

    Mailer provider

--- src/Providers/MailerServiceProvider.php
+++ src/Providers/MailerServiceProvider.php
@@ -5,13 +5,14 @@ namespace Rareloop\Lumberjack\Providers;
 use Rareloop\Lumberjack\Mailer\Transport\WordPressTransport;
 use Symfony\Bridge\Twig\Mime\BodyRenderer;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\Mailer\EventListener\EnvelopeListener;
 use Symfony\Component\Mailer\EventListener\MessageListener;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mailer\TransportInterface;
 use Symfony\Component\Mime\BodyRendererInterface;
-use Symfony\Component\Mime\Email;
 use Twig\Environment;
+use Symfony\Component\Mime\Address;
 
 class MailerServiceProvider extends ServiceProvider
 {
@@ -22,8 +23,12 @@ class MailerServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->set(BodyRendererInterface::class, \DI\create(BodyRenderer::class)->constructor(\DI\get(Environment::class)));
-        $this->app->set('mailer.listener', \DI\create(MessageListener::class)->constructor(null, \DI\get(BodyRendererInterface::class)));
-        $this->app->set('mailer.dispatcher', \DI\create(EventDispatcher::class)->method('addSubscriber', \DI\get('mailer.listener')));
+        $this->app->set('mailer.envelope_listener', \DI\create(EnvelopeListener::class)->constructor(\DI\get('mailer.sender')));
+        $this->app->set('mailer.message_listener', \DI\create(MessageListener::class)->constructor(null, \DI\get(BodyRendererInterface::class)));
+        $this->app->set('mailer.dispatcher', \DI\create(EventDispatcher::class)
+            ->method('addSubscriber', \DI\get('mailer.message_listener'))
+            ->method('addSubscriber', \DI\get('mailer.envelope_listener'))
+        );
         $this->app->set(TransportInterface::class, \DI\create(WordPressTransport::class)->constructor(\DI\get('mailer.dispatcher')));
 
         $this->app->set(MailerInterface::class, \DI\create(Mailer::class)->constructor(
@@ -32,20 +37,9 @@ class MailerServiceProvider extends ServiceProvider
             \DI\get('mailer.dispatcher')
         ));
 
-        // $this->app->singleton('app.mailer.default_to', function () {
-        //     return new Address(\get_option('admin_email'));
-        // });
-
-        // $this->app->singleton('app.mailer.default_from', function () {
-        //     return new Address('ne-pas-repondre@regilait.com', 'Régilait');
-        // });
-
-        // $this->app->singleton('app.mailer.subject_prefix', function () {
-        //     return '[] ';
-        // });
-
-        // $this->app->singleton('app.mailer.renderer', function () {
-        //     return new BodyRenderer($this->app->get('twig'));
-        // });
+        $this->app->singleton('mailer.sender', function () {
+            $sender = new Address('noreply@' . str_replace('www.', '', \parse_url(\home_url(), PHP_URL_HOST)), get_bloginfo('name'));
+            return $this->getConfig('mailer.sender') ?? $sender;
+        });
     }
 }
